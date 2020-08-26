45107aca74908c8981cf97e482493d9567ff934d
Joey Bruno <brunojoey12@gmail.com>
2020-08-26 16:59:07 -0500

    Created sortedTable component to bring that to all uses of tales.

---
+++ client/src/components/Table/sortedTable.js
@@ -0,0 +1,217 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSort } from "@fortawesome/free-solid-svg-icons";
+import "./style.css";
+
+function SortedTable(props) {
+  const { history, games } = props;
+  const [sortConfig, setSortConfig] = useState(null);
+
+  // Sorts the Table
+  let sortedItems = [...games];
+
+  // if (sortConfig !== null) {
+  //   sortedItems.sort((a, b) => {
+  //     if (a[sortConfig] < b[sortConfig]) {
+  //       return -1;
+  //     }
+  //     if (a[sortConfig] > b[sortConfig]) {
+  //       return 1;
+  //     }
+  //     return 0;
+  //   });
+  // };
+
+  if (sortConfig !== null) {
+    let direction;
+    sortedItems.sort((a, b) => {
+      if (a[sortConfig] < b[sortConfig]) {
+        return direction === "ascending" ? 1 : -1;
+      }
+      if (a[sortConfig] > b[sortConfig]) {
+        return direction === "ascending" ? -1 : 1;
+      }
+      return 0;
+    });
+  };
+
+  // const requestSort = (key) => {
+  //   let direction = "ascending";
+  //   if (sortConfig.key === key && sortConfig.direction === "ascending") {
+  //     console.log('key', sortConfig.key);
+  //     direction = "descending";
+  //   }
+  //   setSortConfig({ key, direction });
+  // };
+
+  let newSortedItems;
+  if (games) {
+    newSortedItems = sortedItems.map((sorted) => {
+      return (
+        <tbody key={sorted._id}>
+          <tr>
+            <td
+              style={{
+                textAlign: "center",
+                padding: "1em",
+                borderBottom: "1px solid black",
+              }}
+            >
+              {sorted.hallOfGreat}
+            </td>
+            <td
+              style={{
+                borderBottom: "1px solid black",
+                borderLeft: "1px solid black",
+                padding: ".5em",
+              }}
+            >
+              <span
+                onClick={() => history.push(`/allies/${sorted.ally}`)}
+                className="gameLink"
+              >
+                {sorted.ally}
+              </span>
+            </td>
+            <td
+              style={{
+                borderBottom: "1px solid black",
+                borderLeft: "1px solid black",
+                padding: ".5em",
+              }}
+            >
+              <span
+                onClick={() => history.push(`/games/${sorted._id}`)}
+                className="gameLink"
+              >
+                {sorted.game}
+              </span>
+            </td>
+            <td
+              style={{
+                textAlign: "center",
+                borderBottom: "1px solid black",
+                borderLeft: "1px solid black",
+              }}
+            >
+              {sorted.votes}
+            </td>
+            <td
+              style={{
+                textAlign: "center",
+                borderBottom: "1px solid black",
+                borderLeft: "1px solid black",
+                padding: ".5em",
+              }}
+            >
+              {sorted.victory}
+            </td>
+            <td
+              style={{
+                textAlign: "center",
+                borderBottom: "1px solid black",
+                borderLeft: "1px solid black",
+                padding: ".5em",
+              }}
+            >
+              {sorted.banned}
+            </td>
+          </tr>
+        </tbody>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <table style={{ margin: "0 auto" }} className="table-sortable">
+        <thead>
+          <tr>
+            <th style={{ borderBottom: "1px solid black" }}>
+              Ceremony
+              <FontAwesomeIcon
+                size="1x"
+                style={{ marginLeft: ".25em" }}
+                className="feed-user-icon"
+                icon={faSort}
+                onClick={() => setSortConfig("hallOfGreat")}
+                // onClick={() => requestSort("hallOfGreat")}
+              ></FontAwesomeIcon>
+            </th>
+            <th style={{ borderBottom: "1px solid black" }}>
+              Nominated By
+              <FontAwesomeIcon
+                size="1x"
+                style={{ marginLeft: ".25em" }}
+                className="feed-user-icon"
+                icon={faSort}
+                onClick={() => setSortConfig("ally")}
+                // onClick={() => requestSort("ally")}
+              ></FontAwesomeIcon>
+            </th>
+            <th style={{ borderBottom: "1px solid black" }}>
+              Game
+              <FontAwesomeIcon
+                size="1x"
+                style={{ marginLeft: ".25em" }}
+                className="feed-user-icon"
+                icon={faSort}
+                onClick={() => setSortConfig("game")}
+                // onClick={() => requestSort("game")}
+              ></FontAwesomeIcon>
+            </th>
+            <th
+              style={{
+                borderBottom: "1px solid black",
+              }}
+            >
+              Votes Received
+              <FontAwesomeIcon
+                size="1x"
+                style={{ marginLeft: ".25em" }}
+                className="feed-user-icon"
+                icon={faSort}
+                onClick={() => setSortConfig("votes")}
+                // onClick={() => requestSort("votes")}
+              ></FontAwesomeIcon>
+            </th>
+            <th
+              style={{
+                borderBottom: "1px solid black",
+              }}
+            >
+              Inducted
+              <FontAwesomeIcon
+                size="1x"
+                style={{ marginLeft: ".25em" }}
+                className="feed-user-icon"
+                icon={faSort}
+                onClick={() => setSortConfig("victory")}
+                // onClick={() => requestSort("victory")}
+              ></FontAwesomeIcon>
+            </th>
+            <th
+              style={{
+                borderBottom: "1px solid black",
+              }}
+            >
+              Banned
+              <FontAwesomeIcon
+                size="1x"
+                style={{ marginLeft: ".25em" }}
+                className="feed-user-icon"
+                icon={faSort}
+                onClick={() => setSortConfig("banned")}
+                // onClick={() => requestSort("banned")}
+              ></FontAwesomeIcon>
+            </th>
+          </tr>
+        </thead>
+        {newSortedItems}
+        </table>
+    </div>
+
+  )
+};
+
+export default SortedTable;
--- client/src/pages/Winners.js
+++ client/src/pages/Winners.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import gamesAPI from "../utils/gamesAPI";
-import { Table } from "react-bootstrap";
+import SortedTable from '../components/Table/sortedTable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSort } from '@fortawesome/free-solid-svg-icons';
 import { Typography } from "@material-ui/core";
@@ -40,85 +40,6 @@ function Winners(props) {
 
   //   fetchData();
   // }, [])
-
-  let winnersToRender;
-  if (games) {
-    winnersToRender = games.map((winner) => {
-      return (
-        <tbody key={winner._id}>
-          <tr>
-            <td
-              style={{
-                textAlign: "center",
-                padding: ".5em",
-                borderBottom: "1px solid black",
-              }}
-            >
-              {winner.hallOfGreat}
-            </td>
-            <td
-              style={{
-                borderBottom: "1px solid black",
-                borderLeft: "1px solid black",
-                padding: ".5em",
-              }}
-            >
-              <span onClick={() => history.push(`/allies/${winner.ally}`)} className='gameLink'>{winner.ally}</span>
-            </td>
-            <td
-              style={{
-                borderBottom: "1px solid black",
-                borderLeft: "1px solid black",
-                paddingRight: "2em",
-              }}
-            >
-              <span onClick={() => history.push(`/games/${winner._id}`)} className='gameLink'>{winner.game}</span>
-            </td>
-            <td
-              style={{
-                textAlign: "center",
-                borderBottom: "1px solid black",
-                borderLeft: "1px solid black",
-                // padding: ".5em",
-              }}
-            >
-              {winner.genre}
-            </td>
-            <td
-              style={{
-                textAlign: "center",
-                borderBottom: "1px solid black",
-                borderLeft: "1px solid black",
-                padding: ".5em",
-              }}
-            >
-              {winner.platforms}
-            </td>
-            <td
-              style={{
-                textAlign: "center",
-                borderBottom: "1px solid black",
-                borderLeft: "1px solid black",
-                padding: ".5em",
-              }}
-            >
-              {winner.metacritic}
-            </td>
-            <td
-              style={{
-                textAlign: "center",
-                borderBottom: "1px solid black",
-                borderLeft: "1px solid black",
-              }}
-            >
-              {winner.votes}
-            </td>
-          </tr>
-        </tbody>
-      );
-    });
-  }
-
   
   return (
     <div>
@@ -128,101 +49,7 @@ function Winners(props) {
       >
         The Illustrious Inductees
       </Typography>
-      <Table striped style={{margin: '0 auto' }}>
-        <thead>
-          <tr>
-            <th
-              style={{
-                borderBottom: "1px solid black",
-              }}
-            >
-              Ceremony
-              <FontAwesomeIcon
-                size="1x"
-                style={{marginLeft: '.25em'}}
-                className="feed-user-icon"
-                icon={faSort}
-                // onClick={() => setSortedField()}
-              ></FontAwesomeIcon>
-            </th>
-            <th
-              style={{ borderBottom: "1px solid black" }}
-            >
-              Nominated By
-              <FontAwesomeIcon
-                size="1x"
-                style={{marginLeft: '.25em'}}
-                className="feed-user-icon"
-                icon={faSort}
-              ></FontAwesomeIcon>
-            </th>
-            <th
-              style={{ borderBottom: "1px solid black" }}
-            >
-              Game
-              <FontAwesomeIcon
-                size="1x"
-                style={{marginLeft: '.25em'}}
-                className="feed-user-icon"
-                icon={faSort}
-              ></FontAwesomeIcon>
-            </th>
-            <th
-              style={{
-                borderBottom: "1px solid black",
-              }}
-            >
-              Genre
-              <FontAwesomeIcon
-                size="1x"
-                style={{marginLeft: '.25em'}}
-                className="feed-user-icon"
-                icon={faSort}
-              ></FontAwesomeIcon>
-            </th>
-            <th
-              style={{
-                borderBottom: "1px solid black",
-              }}
-            >
-              Platforms
-              <FontAwesomeIcon
-                size="1x"
-                style={{marginLeft: '.25em'}}
-                className="feed-user-icon"
-                icon={faSort}
-              ></FontAwesomeIcon>
-            </th>
-            <th
-              style={{
-                borderBottom: "1px solid black",
-              }}
-            >
-              Metacritic
-              <FontAwesomeIcon
-                size="1x"
-                style={{marginLeft: '.25em'}}
-                className="feed-user-icon"
-                icon={faSort}
-              ></FontAwesomeIcon>
-            </th>
-            <th
-              style={{
-                borderBottom: "1px solid black",
-              }}
-            >
-              Votes Received
-              <FontAwesomeIcon
-                size="1x"
-                style={{marginLeft: '.25em'}}
-                className="feed-user-icon"
-                icon={faSort}
-              ></FontAwesomeIcon>
-            </th>
-          </tr>
-        </thead>
-        {winnersToRender}
-      </Table>
+      <SortedTable games={games} />
     </div>
   );
 };
