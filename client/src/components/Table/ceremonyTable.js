94231660417f576b2a1c34b32dd66cdbcefce96d
Joey Bruno <brunojoey12@gmail.com>
2020-09-03 10:57:16 -0500

    Changed margin Top for star decoration in ceremony table.

--- client/src/components/Table/ceremonyTable.js
+++ client/src/components/Table/ceremonyTable.js
@@ -172,7 +172,7 @@ function CeremonyTable(props) {
         </thead>
         {newSortedItems}
       </table>
-      <Grid item xs={12} style={{ marginTop: "-4em", marginBottom: '3em' }}>
+      <Grid item xs={12} style={{ marginTop: "-3em", marginBottom: '3em' }}>
           <StarDecoration />
         </Grid>
     </div>
