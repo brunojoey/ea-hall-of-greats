294e83c2aa7d8c2c2e9fa90ac7dfb25306958ed0
Joey Bruno <brunojoey12@gmail.com>
2020-11-17 10:35:08 -0600

    Added comments to variables and uninstalled react materialize and react bootstrap.

--- client/src/components/Dropdown/index.js
+++ client/src/components/Dropdown/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Menu} from "@material-ui/core";
+import { Button, Menu } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import './Dropdown.scss';
 
