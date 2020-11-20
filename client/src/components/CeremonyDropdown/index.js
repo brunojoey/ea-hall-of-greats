bb3909fc100c5a52ee0790ab8df18eb266e6d1a5
Joey Bruno <brunojoey12@gmail.com>
2020-11-20 10:43:57 -0600

    Changed left parameter for both ally and ceremony dropdown button

--- client/src/components/CeremonyDropdown/index.js
+++ client/src/components/CeremonyDropdown/index.js
@@ -72,7 +72,7 @@ function CeremonyDropdown(props) {
   return (
     <div>
       <Button
-        style={{ backgroundColor: 'red', color: "white", marginTop: '1rem', left: '40%' }}
+        style={{ backgroundColor: 'red', color: "white", marginTop: '1rem', left: '37%' }}
         aria-controls="simple-menu"
         aria-haspopup="true"
         onClick={handleClick}
